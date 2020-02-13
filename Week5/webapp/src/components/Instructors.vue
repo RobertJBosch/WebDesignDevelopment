95d5bfeec5a4d002fff51a49917d0fbcf92ecaf0
RobertJBosch <43588544+RobertJBosch@users.noreply.github.com>
2020-02-12 23:09:46 -0800

    Week 5 web app completed minus extra credit

---
+++ Week5/webapp/src/components/Instructors.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="instructors">
+    <a></a>
+    <h1>Instructor Page</h1>
+    <h3>Instructor Data</h3>
+    <table id='instructorTable'>
+            <thead>
+                <tr>
+                    <th class="border">First Name</th>
+                    <th class="border">Middle Initial</th>
+                    <th class="border">Last Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="instructor in instructors" v-bind:key="instructor">
+                    <td class="border">{{ instructor.firstname }}</td>
+                    <td class="border">{{ instructor.middleinitial }}</td>
+                    <td class="border">{{ instructor.lastname }}</td>
+                </tr>
+            </tbody>
+        </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Instructors',
+  mounted() {
+      this.instructors = getInstructors();
+  },
+  data() {
+      return {
+          instructors: []
+      }
+  }
+  
+}
+
+function getInstructors() {
+    return JSON.parse(instructorData);
+}
+var instructorData = '[{"firstname":"Bill Nye", "middleinitial":"T", "lastname":"Science Guy"},' +
+    '{"firstname":"Steve", "middleinitial":"R", "lastname":"Irwin"},' +
+    '{"firstname":"Nathan", "middleinitial":"C", "lastname":"Smurthwaite"}]';
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.datatable {
+    width: 50%;
+    padding: 6px;
+    border-collapse: collapse;
+}
+th.border {
+    border: 1px solid black;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    background-color: red;
+    color: green;
+    text-align: center;
+    padding: 10px;
+}
+td.border {
+    border: 1px solid black;
+    text-align: left;
+
+}
+th {
+    text-align: center;
+    padding: 10px;
+}
+td {
+    text-align: left;
+    padding: 10px;
+}
+h3 {
+    text-align: left;
+}
+</style>
---
+++ Week5/webapp/src/components/Students.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="students">
+    <a></a>
+    <h1>Student Page</h1>
+    <h3>Student Data</h3>
+    <table id='studentTable'>  
+            <thead>
+                <tr>
+                    <th class="border">Student ID Number</th>
+                    <th class="border">Email Address</th>
+                </tr>
+            </thead>       
+            <tbody>
+                <tr v-for="student in students" v-bind:key="student">
+                    <td class="border">{{ student.studentid }}</td>
+                    <td class="border">{{ student.emailaddress }}</td>                
+                </tr>
+            </tbody>
+        </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Students',
+    mounted() {
+        this.students = getStudents();
+    },
+    data () {
+        return {
+            students: []
+        }
+    }
+}
+
+function getStudents() {
+    return JSON.parse(studentData);
+}
+
+var studentData = '[{"studentid":123456, "emailaddress":"thisisanemailaddress@email.com"},' +
+   '{"studentid":789123, "emailaddress": "thisisanotheremail@gmail.com"},' +
+   '{"studentid":456789, "emailaddress": "yetanotherone@hemail.com"}]'
+  
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.datatable {
+    width: 50%;
+    padding: 6px;
+    border-collapse: collapse;
+}
+th.border {
+    border: 1px solid black;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    background-color: red;
+    color: green;
+    text-align: center;
+    padding: 10px;
+}
+td.border {
+    border: 1px solid black;
+    text-align: left;
+    padding: 10px;
+}
+th {
+    text-align: center;
+    padding: 10px;
+}
+td {
+    text-align: left;
+    padding: 10px;
+}
+h3 {
+    text-align: left;
+}
+</style>
